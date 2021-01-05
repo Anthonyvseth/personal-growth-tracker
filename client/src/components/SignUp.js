ca5d1f332cef12d5bd062c09a598a51969528fc9
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-05 10:56:21 -0700

    basic form setup for signup

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,10 +1,59 @@
 import React, {useState} from 'react'
 
-const SignUp = () => {
+const SignUp = (props) => {
+    const [username, setUserame] = useState('')
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setlastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+
 
     return (
         <div>
-        <h1>Get started today!</h1>
+            <h1>Get started today!</h1>
+                <div>
+                    <label>Username</label>
+                        <input 
+                        type = 'text'
+                        name = 'username'
+                        placeholder = 'Enter your username'
+                        />
+                </div>
+                <div>
+                    <label>First Name</label>
+                        <input 
+                        type = 'text'
+                        name = 'first name'
+                        placeholder = 'Enter your fisrt name'
+                        />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                        <input 
+                        type = 'text'
+                        name = 'last name'
+                        placeholder = 'Enter your last name'
+                        />
+                </div>
+                <div>
+                    <label>Email</label>
+                        <input 
+                        type = 'text'
+                        name = 'email'
+                        placeholder = 'Enter your email'
+                        />
+                </div>
+                <div>
+                    <label>Password</label>
+                        <input 
+                        type = 'text'
+                        name = 'password'
+                        placeholder = 'Enter your password'
+                        />
+                </div>
         </div>
     )
 }
