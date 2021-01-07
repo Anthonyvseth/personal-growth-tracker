f642dab7ab6b5098b94c853cd5c7b75cc5a5b30c
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-07 14:39:25 -0700

    Navbar up and running

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import NavbarElements from './NarbarElements'
+import {Nav, NavBarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NarbarElements'
+import { FaBars } from 'react-icons/fa'
+import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div>
-            
+            <Nav>
+                <NavBarContainer>
+                    <NavLogo to='/'>PGT</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                        <NavMenu>
+                            <NavItem>
+                                <NavLink to="/about">About</NavLink>
+                            </NavItem>
+                        </NavMenu>
+                </NavBarContainer>
+            </Nav>
         </div>
     )
 }
 
-export default NavBar
+export default NavBar;
