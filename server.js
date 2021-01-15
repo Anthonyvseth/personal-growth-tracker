ea42fbd9d195353f3d651fde04467cd5ff0f570e
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-15 00:20:51 -0700

    trying to deploy

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
-    app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    )
- }
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
  app.use("/api", AppRouter);
 
 app.listen(PORT, () => console.log(`Server working on Port: ${PORT}`));
