7d00e429e216282126dd723234e41a87b87d281a
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-19 23:04:11 -0700

    trying to deploy jan 19th 2021 3rd try

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const AppRouter = require("./routes/AppRouter");
 
 const PORT = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
- }
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+
 app.use("/api", AppRouter);
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 )
-app.listen(PORT, () => console.log(`Server working on Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
